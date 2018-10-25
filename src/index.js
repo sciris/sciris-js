c6a7a2c70f56027cda929465917fe968d5343b05
Parham Saidi <parham@parha.me>
2018-10-25 13:14:02 +0200

    introduced the vue components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
+import status from './libs/status-service.js';
 import graphs from './libs/graphs.js';
 import rpc from './libs/rpc-service.js';
-import status from './libs/status-service.js';
 import tasks from './libs/task-service.js';
 import user from './libs/user-service.js';
 import utils from './libs/utils.js';
 
-let components = {
-}
+import NotificationStore from './notifications.js';
 
-function install(Vue, options) {
-  if (options && options.components) {
-    options.components.forEach(c => Vue.component(c.name, components[c.name]))
-  } else {
-    Object.keys(components).forEach((key) => {
-      Vue.component(key, components[key])
-    });
-  }
+import Notifications from './components/Notifications.vue';
+import PopupSpinner from './components/PopupSpinner.vue';
+
+function install(Vue) {
+  Object.defineProperty(Vue.prototype, '$notifications', {
+    get () {
+      return NotificationStore
+    }
+  });
+  // Make sure that plugin can be installed only once
+  if (!this.spinnerInstalled) {
+    this.spinnerInstalled = true
+
+    // Create the event bus which allows plugin calls to talk to the 
+    // PopupSpinner object through events.
+    this.eventBus = new Vue()
+
+    // Create the global $spinner functions the user can call 
+    // from inside any component.
+    var that = this;
+    Vue.prototype.$spinner = {
+      start() {
+        // Send a start event to the bus.
+        that.eventBus.$emit('start')
+      }, 
+      stop() {
+        // Send a stop event to the bus.
+        that.eventBus.$emit('stop')
+      }
+    }
+  };
+  Vue.component('Notifications', Notifications);
+  Vue.component('PopupSpinner', PopupSpinner);
 }
 
 // Automatic installation if Vue has been added to the global scope.
@@ -24,7 +48,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  install,
+  install
 }
 
 export {
@@ -33,5 +57,7 @@ export {
   status,
   user,
   tasks,
-  utils
+  utils,
+  PopupSpinner,
+  Notifications
 }
